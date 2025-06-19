05f687d17876eef894f1c74f1957979f432977a6
go52fes <go52fes@mytum.de>
2025-06-19 15:05:57 +0200

    ff

--- include/modules.hpp
+++ include/modules.hpp
@@ -5,6 +5,14 @@ struct Request {
   uint32_t r1, r2, r3, ro;
   uint8_t op;
 };
+ struct Result {
+ uint32 t cycles ;
+ uint32 t signs ;
+ uint32 t overflows ;
+ uint32 t underflows ;
+ uint32 t inextacts ;
+ uint32 t nans;
+ };
 
 extern "C" int run_simulation();
 
